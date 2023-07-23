01b83b352695e7c4277da1624854866c7dfa5be7
NITIN SHARMA <nitinsharma@NITINs-MBP-2.attlocal.net>
2023-07-23 17:03:28 -0500

    Keeping essentials

--- utils/install.sh
+++ utils/install.sh
@@ -21,25 +21,11 @@ brew install plantuml
 # Python development
 brew install python3
 
-# Go development
-brew install go
-
 # IDEs and other guis
 brew install --cask mysql-shell
 brew install --cask quicklook-json
 brew install --cask sequel-ace
 brew install --cask visual-studio-code
-brew install --cask docker
-
-# Compilers, and clis.
-brew install awscli
-brew install gcc
-brew install gettext
-brew install pkg-config
-brew install supabase/tap/supabase
-brew install s3cmd
-
-brew link --force gettext
 
 # Install things we use for collaboration and productivity.
 printf '\nInstalling collaboration and productivity packages...\n'
@@ -52,7 +38,6 @@ brew install --cask google-chrome
 brew install speedtest-cli
 brew install tree
 brew install wget
-brew install fig
 
 ## Productivity Tools
 
@@ -78,18 +63,12 @@ brew install --cask google-drive
 brew install --cask flux 
 brew install --cask keepingyouawake 
 brew install --cask rectangle
-brew install --cask yubico-yubikey-manager 
 brew install --cask maccy
 brew install --cask vlc
-brew install --cask sourcetree
 
-brew install --cask yubico-authenticator
 brew install --cask bitwarden
 
 # Collaboration packages
 brew install --cask discord 
 brew install --cask slack
 brew install --cask zoom
-
-npm i -g \
-    aws-cdk
